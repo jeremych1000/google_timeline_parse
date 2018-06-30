83a6a428d28f0989f3b39b5dccadfbcc46b6dffc
Jeremy Chan <jeremych@outlook.com>
2018-06-30 14:18:34 +0100

    Add offline geocoder support

--- parse.py
+++ parse.py
@@ -1,21 +1,45 @@
 import geopy
 import json
+import reverse_geocoder as rg
 
+from datetime import datetime
+from geopy.geocoders import Nominatim
 
 class entry():
     def __init__(self, timestamp, latitude, longitude):
-        self.timestamp = timestamp
-        self.lat = latitude
-        self.long = longitude
+        timestamp = int(timestamp) / 1000 # google counts in milliseconds
+        latitude = str(latitude)
+        longitude = str(longitude)
+        self.timestamp = datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%SZ')
+        self.latitude = "{}.{}".format(latitude[0:2], latitude[2:])
+        self.longitude = "{}.{}".format(longitude[0:2], longitude[2:])
 
 
-def format_lat_long(lat, long):
+def format_lat_long(latitude, longitude):
     pass
 
 
-def find_country(lat, long):
+def find_country(latitude, longitude, offline=True):
+    if offline:
+        results = rg.search((latitude, longitude))
+        print(results)
+        return results
+        
+    else:
+        geolocator = Nominatim()
+        location = geolocator.reverse("{}, {}".format(latitude, longitude))
+        print(location.address)
+        print((location.latitude, location.longitude))
+        
+        country = location.raw["address"]["country"]
+        
+        print(country)
+        return country
+
+
+def find_day(timestamp):
     pass
- 
+
 
 def read_json(input="test.json"):
     with open(input) as input_json:
@@ -28,7 +52,7 @@ def strip_json(input_json):
     # "longitudeE7" : -1919385,
     # 
     ret = []
-    for line in input_json:
+    for line in input_json["locations"]:
         ret.append(entry(
             timestamp = line["timestampMs"],
             latitude = line["latitudeE7"],
@@ -38,18 +62,15 @@ def strip_json(input_json):
 
 
 def main():
-    from geopy.geocoders import Nominatim
-    geolocator = Nominatim()
-    location = geolocator.reverse("52.509669, 13.376294")
-    print(location.address)
-    print((location.latitude, location.longitude))
-    print(location.raw["address"]["country"])
-
     entries = read_json("test.json")
     print(entries)
 
     entries_parsed = strip_json(entries)
-    print(entries_parsed)
+    print("{} entries parsed.".format(len(entries_parsed)))
+
+    for entry in entries_parsed:
+        country = find_country(entry.latitude, entry.longitude)
+
 
 
 main()
